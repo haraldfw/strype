ff3473992d595f533f750c370f78582c68ae2ccd
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-05-23 08:10:57 +0200

    wip

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from config import Config
+import pyaudio
 
 
 def main():
     config = Config()
     print(vars(config))
+    p = pyaudio.PyAudio()
+    dev_amount = p.get_device_count()
+    for i in range(dev_amount):
+        print(p.get_device_info_by_index(i))
+    # device = p.get_device_info_by_index(config.audio_device)
 
 
 if __name__ == '__main__':
