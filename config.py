901e25c4cb78eb2d8b832c91a5a0711ac736e435
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-05-28 07:50:34 +0200

    removed old visualizer

--- config.py
+++ config.py
@@ -7,14 +7,6 @@ def read_config():
 
 
 class Config:
-    audio_device = -1
-    channels = -1
-    rate = -1
-    bars = -1
-    chunk_size = -1
-    min_frequency = -1
-    max_frequency = -1
-    decay = -1
 
     def __init__(self):
         c = read_config()
@@ -22,6 +14,8 @@ class Config:
         self.channels = c.get('channels', 0)
         self.rate = c.get('rate', 0)
         self.bars = c.get('bars', 0)
+        self.leds_per_bar = c.get('leds_per_bar', 0)
+        self.spacer_leds = c.get('spacer_leds', 0)
         self.chunk_size = c.get('chunk_size', 0)
         self.min_frequency = c.get('min_frequency')
         self.max_frequency = c.get('max_frequency')
